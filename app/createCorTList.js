9398d582c9395039aeba23a861682bac3010ca2a
SparkyJustice <51365995+SparkyJustice@users.noreply.github.com>
2020-11-02 17:23:14 +0000

    remove ctsc from search results for find nearest court

--- app/createCorTList.js
+++ app/createCorTList.js
@@ -5,7 +5,8 @@ const createCorTList = function(serviceArea, singleLocation, centreType) {
   let searchListNames = []
 
   if (singleLocation) {
-    for (let i=0; i < courtDetails.courts.length; i++) {   
+    for (let i=0; i < courtDetails.courts.length; i++) {
+    //   
       if (courtDetails.courts[i].catchment_type === centreType ) { 
 
         for (let j=0; j < courtDetails.courts[i].areas_of_law.length; j++) {
@@ -31,15 +32,18 @@ const createCorTList = function(serviceArea, singleLocation, centreType) {
     for (let i=0; i < courtDetails.courts.length; i++) {   
       for (let j=0; j < courtDetails.courts[i].areas_of_law.length; j++) {
       // does the court handle the service area 
-        if (courtDetails.courts[i].areas_of_law[j] == serviceArea) {    
-          let locationDetails = {
-            name: courtDetails.courts[i].name,
-            slug: courtDetails.courts[i].slug.toLowerCase(),
-            distance: courtDetails.courts[i].distance.toFixed(1),
-            catchment_area : courtDetails.courts[i].catchment_area,
-            catchment_type : courtDetails.courts[i].catchment_type
+        if (courtDetails.courts[i].areas_of_law[j] === serviceArea) { 
+        // exclude ctsc that have a distance of 0 
+          if (courtDetails.courts[i].distance !== 0) {
+            let locationDetails = {
+              name: courtDetails.courts[i].name,
+              slug: courtDetails.courts[i].slug.toLowerCase(),
+              distance: courtDetails.courts[i].distance.toFixed(1),
+              catchment_area : courtDetails.courts[i].catchment_area,
+              catchment_type : courtDetails.courts[i].catchment_type
+            }
+            searchListNames.push(locationDetails)
           }
-          searchListNames.push(locationDetails)
         }
       }      
     }
